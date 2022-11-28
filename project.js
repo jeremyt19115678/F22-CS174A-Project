64d075e79b05cd0c945a1b10e7f2317f80466dc9
Johnny Pan <jpan287@ucla.edu>
2022-11-27 17:36:40 -0800

    Added bird/tree models

--- project.js
+++ project.js
@@ -1,7 +1,8 @@
 import {defs, tiny} from './examples/common.js';
+import {Shape_From_File} from "./examples/obj-file-demo.js";
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Texture, Scene,
 } = tiny;
 
 class Cube_Outline extends Shape {
@@ -76,13 +77,13 @@ const maxWorldY = 20;
 const maxWorldZ = 20;
 const birdRadius = 0.8;
 const spawnRadius = 5;
-const lightColor = hex_color("#f5d20c");
+const lightColor = hex_color("#ffffff");
 
 class Bird {
     constructor() {
         this.maxSpeed = birdRadius * 8; // per second
         this.maxForceComponent = this.maxSpeed * 0.02; // per second
-        this.maxForceMultiplier = 3.5;
+        this.maxForceMultiplier = 1;
         this.attentionRadius = birdRadius * 3;
         this.position = vec3(spawnRadius * Math.random() - spawnRadius / 2 + maxWorldX / 2, spawnRadius * Math.random() - spawnRadius / 2 + maxWorldY / 2, spawnRadius * Math.random() - spawnRadius / 2 + maxWorldZ / 2); // initialized at random position in the middle of the world
         this.velocity = vec3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalized().times(this.maxSpeed); // initialized with random velocity
@@ -189,6 +190,13 @@ class Bird {
     }
 }
 
+class Tree {
+    constructor() {
+        this.attentionRadius = birdRadius * 3;
+        this.position = vec3(0, 0, 0); // initialized at random position in the middle of the world
+    }
+}
+
 export class Project extends Scene {
     constructor() {
         // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
@@ -205,48 +213,115 @@ export class Project extends Scene {
             cone_bird: new defs.Closed_Cone(10, 10),
             line_segment: new Line(),
             floor: new defs.Square(),
+            bird_model: new Shape_From_File("assets/Bird.obj"),
+            tree_1: new Shape_From_File("assets/Tree.obj"),
+            tree_2: new Shape_From_File("assets/Tree_2.obj"),
+            tree_3: new Shape_From_File("assets/Tree_3.obj"),
+            tree_4: new Shape_From_File("assets/Tree_4.obj"),
+            tree_5: new Shape_From_File("assets/Tree_5.obj"),
+            tree_6: new Shape_From_File("assets/Tree_6.obj"),
         };
 
         // *** Materials
         this.materials = {
             white: new Material(new defs.Basic_Shader()),
-            test: new Material(new defs.Phong_Shader(), {ambient: 0.15, diffusivity: 1.0, specularity: 0, color: hex_color("#ffffff")})
+            test: new Material(new defs.Phong_Shader(), {
+                ambient: 0.15,
+                diffusivity: 1.0,
+                specularity: 0,
+                color: hex_color("#ffffff")
+            }),
         }
+
+        // Bump map
+        this.bumps = {
+            bird: new Material(new defs.Fake_Bump_Map(1), {
+                color: color(.5, .5, .5, 1),
+                ambient: .3,
+                diffusivity: .5,
+                specularity: .5,
+                texture: new Texture("assets/1846471280_1024x1024_6195987693227547908.png")
+            }),
+            tree_1: new Material(new defs.Fake_Bump_Map(1), {
+                color: color(.5, .5, .5, 1),
+                ambient: .3,
+                diffusivity: .5,
+                specularity: .5,
+                texture: new Texture("assets/2481502052_2048x2048_6195987693227547908.png")
+            }),
+            tree_2: new Material(new defs.Fake_Bump_Map(1), {
+                color: color(.5, .5, .5, 1),
+                ambient: .3,
+                diffusivity: .5,
+                specularity: .5,
+                texture: new Texture("assets/220389868_2048x2048_6195987693227547908.png")
+            }),
+            tree_3: new Material(new defs.Fake_Bump_Map(1), {
+                color: color(.5, .5, .5, 1),
+                ambient: .3,
+                diffusivity: .5,
+                specularity: .5,
+                texture: new Texture("assets/1350945960_2048x2048_6195987693227547908.png")
+            }),
+            tree_4: new Material(new defs.Fake_Bump_Map(1), {
+                color: color(.5, .5, .5, 1),
+                ambient: .3,
+                diffusivity: .5,
+                specularity: .5,
+                texture: new Texture("assets/3384801072_2048x2048_6195987693227547908.png")
+            }),
+            tree_5: new Material(new defs.Fake_Bump_Map(1), {
+                color: color(.5, .5, .5, 1),
+                ambient: .3,
+                diffusivity: .5,
+                specularity: .5,
+                texture: new Texture("assets/110997054_2048x2048_6195987693227547908.png")
+            }),
+            tree_6: new Material(new defs.Fake_Bump_Map(1), {
+                color: color(.5, .5, .5, 1),
+                ambient: .3,
+                diffusivity: .5,
+                specularity: .5,
+                texture: new Texture("assets/3275408258_2048x2048_6195987693227547908.png")
+            }),
+        };
 
         this.initial_camera_location = Mat4.look_at(vec3(maxWorldX / 2, maxWorldY * 1.5, maxWorldZ * 3), vec3(maxWorldX/2, maxWorldY/2, maxWorldZ/2), vec3(0, 1, 0));
         this.birds = Array(25);
         for (let i = 0; i < this.birds.length; i++) {
             this.birds[i] = new Bird();
         }
+        this.trees = Array();
+        this.paused = false;
     }
 
     make_control_panel() {
         // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
         this.key_triggered_button("Increase Separation", ["Control", "1"], () => {
-            separationMultiplier = Math.min(separationMultiplier + 0.5, 10);
+            separationMultiplier = Math.min(separationMultiplier + 0.25, 10);
             console.log(`separationMultiplier = ${separationMultiplier}, alignmentMultiplier = ${alignmentMultiplier}, cohesionMultiplier = ${cohesionMultiplier}`);
         });
         this.key_triggered_button("Decrease Separation", ["Control", "2"], () => {
-            separationMultiplier = Math.max(separationMultiplier - 0.5, 0);
+            separationMultiplier = Math.max(separationMultiplier - 0.25, 0);
             console.log(`separationMultiplier = ${separationMultiplier}, alignmentMultiplier = ${alignmentMultiplier}, cohesionMultiplier = ${cohesionMultiplier}`);
         });
         this.new_line();
         this.key_triggered_button("Increase Alignment", ["Control", "3"], () => {
-            alignmentMultiplier = Math.min(alignmentMultiplier + 0.5, 10);
+            alignmentMultiplier = Math.min(alignmentMultiplier + 0.25, 10);
             console.log(`separationMultiplier = ${separationMultiplier}, alignmentMultiplier = ${alignmentMultiplier}, cohesionMultiplier = ${cohesionMultiplier}`);
         });
         this.key_triggered_button("Decrease Alignment", ["Control", "4"], () => {
-            alignmentMultiplier = Math.max(alignmentMultiplier - 0.5, 0);
+            alignmentMultiplier = Math.max(alignmentMultiplier - 0.25, 0);
             console.log(`separationMultiplier = ${separationMultiplier}, alignmentMultiplier = ${alignmentMultiplier}, cohesionMultiplier = ${cohesionMultiplier}`);
         });
         this.new_line();
         this.key_triggered_button("Increase Cohesion", ["Control", "5"], () => {
-            cohesionMultiplier = Math.min(cohesionMultiplier + 0.5, 10);
+            cohesionMultiplier = Math.min(cohesionMultiplier + 0.25, 10);
             console.log(`separationMultiplier = ${separationMultiplier}, alignmentMultiplier = ${alignmentMultiplier}, cohesionMultiplier = ${cohesionMultiplier}`);
         });
         this.new_line();
         this.key_triggered_button("Decrease Cohesion", ["Control", "6"], () => {
-            cohesionMultiplier = Math.max(cohesionMultiplier - 0.5, 0);
+            cohesionMultiplier = Math.max(cohesionMultiplier - 0.25, 0);
             console.log(`separationMultiplier = ${separationMultiplier}, alignmentMultiplier = ${alignmentMultiplier}, cohesionMultiplier = ${cohesionMultiplier}`);
         });
         this.key_triggered_button("Add bird", ["Control", "z"], () => {
@@ -257,8 +332,24 @@ export class Project extends Scene {
             this.birds.pop();
             console.log('Bird removed');
         });
-    }
+        this.key_triggered_button("Pause", ["Control", " "], () => {
+            this.paused = !this.paused;
+        });
 
+
+        this.key_triggered_button("Add Tree", ["Shift", "T"], () => {
+            let newTree = new Tree();
+            let shapeKeys = Object.keys(this.shapes);
+            // let treeNum = Math.random() * 6 << 0;
+            let treeNum = 0;
+            newTree.shape = this.shapes[shapeKeys[10 + treeNum]];
+            let bumpKeys = Object.keys(this.bumps);
+            newTree.bump = this.bumps[bumpKeys[1 + treeNum]];
+            this.trees.push(newTree);
+
+            console.log(treeNum+1);
+        })
+    }
 
     rotateAlign(v1, v2)
     {
@@ -318,10 +409,18 @@ export class Project extends Scene {
             .times(Mat4.translation(bird.position[0] / birdRadius, bird.position[1] / birdRadius, bird.position[2] / birdRadius))
             .times(this.rotateAlign(vec3(0, 0, 1), vec3(bird.velocity[0], bird.velocity[1], bird.velocity[2]).normalized()));
             // console.log(bird.position);
-            this.shapes.cone_bird.draw(context, program_state, birdBasis, this.materials.test);
-            bird.updateMotion(this.birds, dt);
+            this.shapes.bird_model.draw(context, program_state, birdBasis, this.bumps.bird);
+            if (!this.paused) {
+                bird.updateMotion(this.birds, dt);
+            }
             
-            this.shapes.line_segment.draw(context, program_state, birdBasis.times(Mat4.scale(2, 2, 2)), this.materials.white, "LINES");
+            //this.shapes.line_segment.draw(context, program_state, birdBasis.times(Mat4.scale(2, 2, 2)), this.materials.white, "LINES");
         });
+
+        // draw the trees
+        this.trees.forEach(tree => {
+            let treeBasis = root;
+            tree.shape.draw(context, program_state, treeBasis, tree.bump);
+        })
     }
 }
